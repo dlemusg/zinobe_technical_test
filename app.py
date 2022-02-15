b18161b86042a94a610407e47534e6cb05624f9f
David Lemus <31749893+dlemusg@users.noreply.github.com>
2022-02-15 10:52:57 -0500

    Metodo para obtener los paises y sus lenguajes por region

--- app.py
+++ app.py
@@ -1,3 +1,35 @@
 from turtle import pd
 import pandas as pd
 import requests
+
+
+def get_regions():
+    return 0
+
+def get_countries_by_region(region):
+    countries = requests.get(f'https://restcountries.com/v3.1/region/{region}').json()
+
+    final_countries = []
+
+    for country in countries:
+        country = {'region': region, 
+                    'country': country['name']['official'], 
+                    'languaje': country['languages']}
+        final_countries.append(country)
+
+    
+    return final_countries
+
+
+
+
+
+
+
+#regions = get_regions()
+#regions = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
+regions = ['Africa']
+
+for region in regions:
+    print(get_countries_by_region(region))
+
