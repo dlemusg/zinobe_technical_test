6df2661700a15ca5421c034f33d9dbcf5f8796ee
David Lemus <31749893+dlemusg@users.noreply.github.com>
2022-02-15 19:31:25 -0500

    Se estructura el codigo para mayor legibilidad

--- app.py
+++ app.py
@@ -4,145 +4,28 @@ OK ::: En la columna Time ponga el tiempo que tardo en armar la fila (debe ser a
 OK ::: La tabla debe ser creada en un DataFrame con la libreria PANDAS
 OK ::: Con funciones de la libreria pandas muestre el tiempo total, el tiempo promedio, el tiempo minimo y el maximo que tardo en procesar toda las filas de la tabla.
 Guarde el resultado en sqlite.
-Genere un Json de la tabla creada y guardelo como data.json
+OK ::: Genere un Json de la tabla creada y guardelo como data.json
 La prueba debe ser entregada en un repositorio git.
 """
 
-import pandas as pd
-import requests
-import timeit
-from hashlib import sha1
-import json
+from core_app import get_coutries_by_region, get_all_countries, create_df_and_calc, table_to_json
 
 
+if __name__ == "__main__":
+
+    print(create_df_and_calc(get_all_countries()))
 
 
-def get_coutries_by_region(region):
-    countries = requests.get(
-        f'https://restcountries.com/v3.1/region/{region}').json()
-    return 0
-
-
-def get_all_countries():
-
-    """
-        Description
-        ---------
-        Obtiene la información de todos los paises expuestos en la api 
-
-        Parameters
-        ----------
-        
-        Returns
-        -------
-        tipo
-            Lista de diccionarios con:
-                Región: Región de pais
-                Country: Nombre del pais
-                Lenguaje: Idioma principal (encriptado en sha)
-                Time: Tiempo que demoro la fila
-    """
-
-    countries = requests.get(f'https://restcountries.com/v3.1/all').json()
-
-    final_countries = []
-
-    for country in countries:
-        start = timeit.timeit()
-        region = country['region']
-
-        try:
-            encode_text = encode_language(
-                list(country['languages'].values())[0])
-        except:
-            encode_text = ''
-
-        country_text = country['name']['official']
-        end = timeit.timeit()
-        total = (end-start)*1000
-
-        country = {'region': region,
-                   'country': country_text,
-                   'language': encode_text,
-                   'time': total}
-
-        final_countries.append(country)
-
-    return final_countries
-
-
-def encode_language(language):
-    """
-        Description
-        ---------
-        Encripta en formato SHA una cadena
-
-        Parameters
-        languaje: Cadena a encriptar
-        
-        Returns
-        -------
-        Text
-            Cadena encriptada
-    """
-    return sha1(language.encode('utf-8')).hexdigest()
-
-
-def create_df_and_calc(result):
-
-    """
-        Description
-        ---------
-        Crea un df a partir de una lista de diccionario y calcula el total, promedio, valor minimo y max 
-        de todos los registros utilizando funciones de la libreria pandas 
-
-        Parameters
-        result: Lista de diccionario con los registros para el df
-        
-        Returns
-        -------
-        Float
-            total: Sumatoria del total de tiempos
-            mean: Promedio de todos los tiempos
-            min: Valor más pequeño de todos los tiempos
-            max: Valor más grande de todos los tiempos
-    """
-
-    df = pd.DataFrame(result)
-
-    total = df["time"].sum()
-    mean = df["time"].mean()
-    min = df["time"].min()
-    max = df["time"].max()
-
-    return total, mean, min, max
-
-
-def table_to_json(result):
-    """
-        Description
-        ---------
-        Genere un Json de la tabla creada y lo guarda como data.json
-        
-        Parameters
-        ----------
-        result: Valores de la tabla a convertir en .json
-        
-        Returns
-        -------
-        
-    """
-    with open('data.json', 'w') as file:
-        json.dump(result, file)
-
-
-#regions = get_regions()
-#regions = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
-#regions = ['Africa']
+# regions = get_regions()
+# regions = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
+# regions = ['Africa']
 
 # for region in regions:
     # print(get_countries_by_region(region))
 
-print(create_df_and_calc(get_all_countries()))
+#print(create_df_and_calc(get_all_countries()))
 
-table_to_json(get_all_countries())
+#print(create_df_and_calc(get_coutries_by_region(['africa'])))
+#print(get_coutries_by_region(['Africa']))
+
+#table_to_json(get_all_countries())
---
+++ core_app.py
@@ -0,0 +1,142 @@
+import pandas as pd
+import requests
+import timeit
+from hashlib import sha1
+import json
+
+
+
+
+def get_coutries_by_region(regions):
+
+    final_countries = []
+
+    for region in regions:
+        try:
+            countries = requests.get(
+                f'https://restcountries.com/v3.1/region/{region}').json()
+
+            final_countries += final_countrie_list(countries)           
+
+        except:
+            next
+
+    return final_countries
+
+
+def get_all_countries():
+    """
+        Description
+        ---------
+        Obtiene la información de todos los paises expuestos en la api
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+        tipo
+            Lista de diccionarios con:
+                Región: Región de pais
+                Country: Nombre del pais
+                Lenguaje: Idioma principal (encriptado en sha)
+                Time: Tiempo que demoro la fila
+    """
+
+    countries = requests.get(f'https://restcountries.com/v3.1/all').json()
+
+    
+
+    return final_countrie_list(countries)
+
+
+def final_countrie_list(countries):
+
+    final_countries = []
+
+    for country in countries:
+        start = timeit.timeit()
+        region = country['region']
+
+        try:
+            encode_text = encode_language(
+                list(country['languages'].values())[0])
+        except:
+            encode_text = ''
+
+        country_text = country['name']['official']
+        end = timeit.timeit()
+        total = (end-start)*1000
+
+        country = {'region': region,
+                    'country': country_text,
+                    'language': encode_text,
+                    'time': total}
+
+        final_countries.append(country)
+
+    return final_countries
+
+
+def encode_language(language):
+    """
+        Description
+        ---------
+        Encripta en formato SHA una cadena
+
+        Parameters
+        languaje: Cadena a encriptar
+
+        Returns
+        -------
+        Text
+            Cadena encriptada
+    """
+    return sha1(language.encode('utf-8')).hexdigest()
+
+
+def create_df_and_calc(result):
+    """
+        Description
+        ---------
+        Crea un df a partir de una lista de diccionario y calcula el total, promedio, valor minimo y max 
+        de todos los registros utilizando funciones de la libreria pandas 
+
+        Parameters
+        result: Lista de diccionario con los registros para el df
+
+        Returns
+        -------
+        Float
+            total: Sumatoria del total de tiempos
+            mean: Promedio de todos los tiempos
+            min: Valor más pequeño de todos los tiempos
+            max: Valor más grande de todos los tiempos
+    """
+
+    df = pd.DataFrame(result)
+
+    total = df["time"].sum()
+    mean = df["time"].mean()
+    min = df["time"].min()
+    max = df["time"].max()
+
+    return total, mean, min, max
+
+
+def table_to_json(result):
+    """
+        Description
+        ---------
+        Genere un Json de la tabla creada y lo guarda como data.json
+
+        Parameters
+        ----------
+        result: Valores de la tabla a convertir en .json
+
+        Returns
+        -------
+
+    """
+    with open('data.json', 'w') as file:
+        json.dump(result, file)
